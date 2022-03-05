d4976a156e928deec3792b416749c178acd1e5f5
Kamyyyk <kamyk962@gmail.com>
2022-03-05 11:50:18 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(funkcja(10,100));
+    }
+
+    private static int funkcja(int i, int a) {
+        return (i * a);
+    }
+
+}
