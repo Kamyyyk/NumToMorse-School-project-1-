44ed653eec91bf4638c161a563dfa57739ffeff9
Kamyyyk <kamyk962@gmail.com>
2022-03-12 10:44:19 +0100

    wip

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(funkcja(10,100));
+    public static void main(String[] args) throws IOException {
+
+
+        char[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        String[] morse = {"-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String numberInput = reader.readLine();
+
+        try {
+            System.out.println((numtomorse(numberInput, numbers, morse)));
+        } catch (Exception e) {
+            System.out.println("Blad" + e);
+        }
     }
 
-    private static int funkcja(int i, int a) {
-        return (i * a);
-    }
 
+    public static StringBuilder numtomorse(String numberInput, char[] numbers, String[] morse){
+        StringBuilder answer = new StringBuilder();
+        numberInput = numberInput.toLowerCase();
+        char[] morsec = numberInput.toCharArray();
+        for (int i = 0; i < morsec.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[j] == morsec[i]) {
+                    answer.append(morse[j]);
+                }
+            }
+        }
+        System.out.println(numberInput + " na kod morse'a to:");
+         return answer;
+    }
 }
